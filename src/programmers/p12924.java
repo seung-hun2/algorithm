eb87d53d8410b53d6a4169507c9210d465ca8781
seung-hun2 <jungsh2328@naver.com>
2023-07-14 22:52:00 +0900

    프로그래머스 - 숫자의 표현

---
+++ src/programmers/p12924.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class p12924 {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+
+}
