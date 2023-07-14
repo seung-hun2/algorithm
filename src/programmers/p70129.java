1fe812e42dc6586f0e8e298ec476630b1a1a1b1a
seung-hun2 <jungsh2328@naver.com>
2023-07-14 22:37:27 +0900

    프로그래머스 - 이진 변환 반복하기

---
+++ src/programmers/p70129.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class p70129 {
+
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while (s.length() > 1) {
+            int cnt = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    answer[1]++;
+                } else {
+                    cnt++;
+                }
+            }
+            s = Integer.toBinaryString(cnt);
+            answer[0]++;
+        }
+        return answer;
+    }
+
+}
