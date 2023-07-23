017f5aec3c82d7abcf45381c4eacec2ac667a876
seung-hun2 <jungsh2328@naver.com>
2023-07-23 22:32:58 +0900

    프로그래머스 - 멀리 뛰기

---
+++ src/programmers/p12914.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class p12914 {
+    public long solution(int n) {
+        long answer = 0;
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<2001;i++){
+            dp[i] = (dp[i-2] + dp[i-1])%1234567;
+        }
+
+        return dp[n];
+    }
+
+}
