69985cf7c2847eed1e71db554fa0a740d2b0e4b2
seung-hun2 <jungsh2328@naver.com>
2023-07-26 15:15:13 +0900

    프로그래머스 - 연속 부분 수열 합의 개수

---
+++ src/programmers/p131701.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class p131701 {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+
+        int start = 1;
+        while (start <= elements.length) {
+            for (int i = 0; i < elements.length; i++) {
+                int value = 0;
+                for (int j = i; j < i + start; j++) {
+                    value += elements[j % elements.length];
+                }
+                set.add(value);
+            }
+            start++;
+        }
+
+        return set.size();
+    }
+
+}
