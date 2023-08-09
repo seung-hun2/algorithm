ca2b6740265963813ceb84e148b8c0743faa7e75
seung-hun2 <jungsh2328@naver.com>
2023-08-09 22:49:09 +0900

    프로그래머스 - 택배상자

---
+++ src/programmers/p131704.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.Stack;
+
+public class p131704 {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int size = order.length;
+        int index = 0;
+        for(int i=1;i<=size;i++){
+            if(order[index] != i){
+                stack.push(i);
+            }else{
+                index++;
+                answer++;
+            }
+            while(!stack.isEmpty() && stack.peek()== order[index]){
+                stack.pop();
+                index++;
+                answer++;
+            }
+        }
+        return answer++;
+    }
+
+}
