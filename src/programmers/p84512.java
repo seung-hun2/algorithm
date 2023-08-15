fffc250deb4848deaaf6d20c1479600d55bb78cd
seung-hun2 <jungsh2328@naver.com>
2023-08-15 21:04:50 +0900

    프로그래머스 - 모음 사전

---
+++ src/programmers/p84512.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class p84512 {
+    class Solution {
+        public int solution(String word) {
+            String str = "AEIOU";
+            int[] x = {781,156,31,6,1};
+            int index, result = word.length();
+            for(int i=0;i<word.length();i++){
+                index = str.indexOf(word.charAt(i));
+                result += x[i] * index;
+            }
+            return result;
+        }
+    }
+
+}
