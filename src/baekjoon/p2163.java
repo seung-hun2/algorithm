45d8606a633527d01207905ecb1aff607cc283d0
seung-hun2 <jungsh2328@naver.com>
2023-09-19 22:33:35 +0900

    백준 - 초콜릿 자르기

---
+++ src/baekjoon/p2163.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        System.out.println(N*M-1);
+    }
+
+}
