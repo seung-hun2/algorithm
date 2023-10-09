be8cfb98e5a3b430b0ba4fdda7d2e592b89e69ed
seung-hun2 <jungsh2328@naver.com>
2023-10-09 17:09:57 +0900

    백준 - N과 M (6)

---
+++ src/baekjoon/p15655.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p15655 {
+
+    static int N,M;
+    static int[] numbers,output;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        numbers = new int[N];
+        output = new int[M];
+        check = new boolean[N];
+        for(int i=0;i<N;i++) numbers[i] = sc.nextInt();
+        Arrays.sort(numbers);
+
+        perm(0,0);
+    }
+
+    public static void perm(int depth, int start){
+        // base case
+        if(depth == M){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<M;i++){
+                sb.append(output[i]).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+        // recursive case
+        for(int i=start;i<N;i++){
+            if(!check[i]){
+                check[i] = true;
+                output[depth] = numbers[i];
+                perm(depth+1, i + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
