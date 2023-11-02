4e703e6532cf5b4ac20db217c0fa5c5839f12553
seung-hun2 <jungsh2328@naver.com>
2023-11-02 14:11:58 +0900

    프로그래머스 - 큰 수 만들기

---
+++ src/programmers/p42883.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class p42883 {
+    class Solution {
+        public String solution(String number, int k) {
+            String answer = "";
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            for(int i=0;i<number.length()-k;i++){
+                int max = 0;
+                for(int j=index;j<=i+k;j++){
+                    if(max < number.charAt(j) - '0'){
+                        max = number.charAt(j) - '0';
+                        index = j+1;
+                    }
+                }
+                sb.append(max);
+            }
+            return sb.toString();
+        }
+    }
+
+}
