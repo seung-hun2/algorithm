2ee3049d5fb0c5df1efcd944c1ec73916f992d43
seung-hun2 <jungsh2328@naver.com>
2023-11-03 17:37:15 +0900

    프로그래머스 - 메뉴 리뉴얼

---
+++ src/programmers/p72411.java
@@ -0,0 +1,56 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class p72411 {
+    class Solution {
+        List<String> answerList = new ArrayList<>();
+        Map<String, Integer> hashMap = new HashMap<>();
+        public String[] solution(String[] orders, int[] course) {
+            for(int i=0;i<orders.length;i++){
+                char[] arr = orders[i].toCharArray();
+                Arrays.sort(arr);
+                orders[i] = String.valueOf(arr);
+            }
+
+            for(int courseLength : course){
+                for(String order : orders){
+                    combination("",order,courseLength);
+                }
+                if(!hashMap.isEmpty()){
+                    List<Integer> countList = new ArrayList<>(hashMap.values());
+                    int max = Collections.max(countList);
+                    if(max > 1)
+                        for(String key : hashMap.keySet())
+                            if(hashMap.get(key) == max)
+                                answerList.add(key);
+
+                    hashMap.clear();
+                }
+            }
+            Collections.sort(answerList);
+            String[] answer = new String[answerList.size()];
+            for(int i=0;i<answer.length;i++)
+                answer[i] = answerList.get(i);
+            return answer;
+        }
+
+        public void combination(String order, String others, int count){
+            // base case
+            if(count == 0 ){
+                hashMap.put(order,hashMap.getOrDefault(order,0) + 1);
+                return;
+            }
+            // recursive case
+            for(int i=0;i<others.length();i++){
+                combination(order+others.charAt(i), others.substring(i+1), count-1);
+            }
+        }
+    }
+
+}
