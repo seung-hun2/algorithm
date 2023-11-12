efbb7fb786ae1ff501fd65077e2f7d5836b288c2
seung-hun2 <jungsh2328@naver.com>
2023-11-12 23:02:07 +0900

    프로그래머스 - 핸드폰 번호 가리기

---
+++ src/programmers/p12948.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class p12948 {
+    class Solution {
+        public String solution(String phone_number) {
+            String answer = "";
+            char[] ch = phone_number.toCharArray();
+            for(int i=0;i<ch.length-4;i++){
+                ch[i] = '*';
+            }
+            return String.valueOf(ch);
+        }
+    }
+
+}
