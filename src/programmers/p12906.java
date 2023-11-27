2fe0f8206238e1d9a4c67ba46866fb444b843410
seung-hun2 <jungsh2328@naver.com>
2023-11-27 21:30:33 +0900

    프로그래머스 - 같은 숫자는 싫어

---
+++ src/programmers/p12906.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class p12906 {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        int value = -1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] != value){
+                list.add(arr[i]);
+                value = arr[i];
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+}
