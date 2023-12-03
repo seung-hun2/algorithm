80cf2bc30ecb986e679143a80a093546a241045f
seung-hun2 <jungsh2328@naver.com>
2023-12-03 17:41:44 +0900

    프로그래머스 - 콜라 문제

---
+++ src/programmers/p132267.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class p132267 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n>=a){
+            answer += n/a * b;
+            n = (n/a * b) + n%a;
+        }
+        return answer;
+    }
+
+}
