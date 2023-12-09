5f9dce2b38731cfbbc9a2067372ab403ed1dfb90
seung-hun2 <jungsh2328@naver.com>
2023-12-09 15:30:50 +0900

    백준 - 순열 사이클

---
+++ src/baekjoon/p10451.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class p10451 {
+
+    static int N;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc=0;tc<T;tc++){
+            N = sc.nextInt();
+            int[] arr = new int[N];
+            boolean[] visited = new boolean[N];
+            for(int i=0;i<N;i++){
+                arr[i] = sc.nextInt()-1;
+            }
+            int ans = 0 ;
+            for(int i=0;i<N;i++){
+                if(visited[i]) continue;
+                visited[i] = true;
+                ans++;
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                while (!q.isEmpty()){
+                    int next = q.poll();
+                    if(!visited[arr[next]]){
+                        q.add(arr[next]);
+                        visited[arr[next]] = true;
+                    }
+                }
+            }
+            System.out.println(ans);
+
+        }
+    }
+
+}
