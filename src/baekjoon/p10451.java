fb77ace2a487d68abd98a79d8382484e4326ef33
seung-hun2 <jungsh2328@naver.com>
2023-12-11 23:14:06 +0900

    백준 - 순열 사이클 (dfs 풀이 추가)

--- src/baekjoon/p10451.java
+++ src/baekjoon/p10451.java
@@ -7,17 +7,24 @@ import java.util.Scanner;
 public class p10451 {
 
     static int N;
+    static int[] arr;
+    static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for(int tc=0;tc<T;tc++){
             N = sc.nextInt();
-            int[] arr = new int[N];
+            int ans = 0;
+
+            /*
+            bfs code
+
+            arr = new int[N];
+
             boolean[] visited = new boolean[N];
             for(int i=0;i<N;i++){
                 arr[i] = sc.nextInt()-1;
             }
-            int ans = 0 ;
             for(int i=0;i<N;i++){
                 if(visited[i]) continue;
                 visited[i] = true;
@@ -32,9 +39,30 @@ public class p10451 {
                     }
                 }
             }
+            */
+            arr = new int[N+1];
+            visited = new boolean[N+1];
+            for(int i=1;i<=N;i++){
+                arr[i] = sc.nextInt();
+            }
+            for(int i=1;i<=N;i++){
+                if(!visited[i]){
+                    dfs(i);
+                    ans++;
+                }
+            }
+
             System.out.println(ans);
 
         }
+
+    }
+    static void dfs(int node){
+        visited[node] = true;
+        if(!visited[arr[node]]){
+            dfs(arr[node]);
+        }
+
     }
 
 }
