289ed299ecbcb79e2cca7990959a96a3b2d4582e
seung-hun2 <jungsh2328@naver.com>
2024-01-12 15:39:40 +0900

    프로그래머스 - 예산

---
+++ src/programmers/p12982.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class p12982 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0;i<d.length;i++){
+            budget -= d[i];
+            if(budget < 0) break;
+            answer++;
+        }
+        return answer;
+    }
+
+}
