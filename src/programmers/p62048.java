bd2786ff629bc35bbb1c10cb691f63cf0458b261
seung-hun2 <jungsh2328@naver.com>
2024-02-15 12:21:13 +0900

    프로그래머스 - 멀쩡한 사각형

---
+++ src/programmers/p62048.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class p62048 {
+    public long solution(int w, int h)
+    {
+        long ref = gcd(w, h);
+
+        return ((long) w * h) - (((w / ref) + (h / ref) - 1) * ref);
+    }
+
+    private int gcd(int n, int m)
+    {
+        while (m != 0)
+        {
+            int r = n % m;
+
+            n = m;
+            m = r;
+        }
+
+        return n;
+    }
+
+}
