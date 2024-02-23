10612b2df500ee2701cc1d3cb019b74337bdd4a2
seung-hun2 <jungsh2328@naver.com>
2024-02-23 16:35:55 +0900

    프로그래머스 - 음양 더하기

---
+++ src/programmers/p76501.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class p76501 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+
+}
