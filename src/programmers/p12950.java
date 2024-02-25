a76b2507ab7684d45849a5cee22865293d81e964
seung-hun2 <jungsh2328@naver.com>
2024-02-25 21:58:16 +0900

    프로그래머스 - 행렬의 덧셈

---
+++ src/programmers/p12950.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class p12950 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for(int i=0 ; i<arr1.length ; i++) {
+            for(int j=0 ; j<arr1[0].length ; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
+    }
+
+}
