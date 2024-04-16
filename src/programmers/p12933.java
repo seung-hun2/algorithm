ea3ee2a2cc09c8c3507a70f791ad1c9e75ff44a0
seung-hun2 <jungsh2328@naver.com>
2024-04-17 00:24:25 +0900

    프로그래머스 - 정수 내림차순으로 배치하기

---
+++ src/programmers/p12933.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class p12933 {
+    public long solution(long n) {
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+
+        return Long.parseLong(sb.reverse().toString());
+    }
+
+}
