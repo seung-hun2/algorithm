07c286ff7fdf66a7cc4c387cd2ff3aa0cce85eff
seung-hun2 <jungsh2328@naver.com>
2024-04-18 21:31:13 +0900

    프로그래머스 - x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/p12954.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class p12954 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num;
+            num += x;
+        }
+        return answer;
+    }
+
+}
