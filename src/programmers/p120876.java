eaa7f927b1859d77ba1aef6929db77f4893bf5d4
seung-hun2 <jungsh2328@naver.com>
2024-05-03 17:07:44 +0900

    프로그래머스 - 겹치는 선분의 길이

---
+++ src/programmers/p120876.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class p120876 {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] count = new int[201];
+        for(int[] i : lines){
+            int start = i[0] + 100;
+            int end = i[1] + 100;
+
+            for(int k=start;k<end;k++) count[k] +=1;
+        }
+
+        for(int i:count) if(i>1) answer++;
+
+        return answer;
+    }
+
+}
