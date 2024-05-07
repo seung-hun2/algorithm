34d0d72d60543df8f6c88c563d1d087d2a964831
seung-hun2 <jungsh2328@naver.com>
2024-05-07 20:29:19 +0900

    프로그래머스 - 저주의 숫자 3

---
+++ src/programmers/p120871.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class p120871 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer++;
+            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+}
