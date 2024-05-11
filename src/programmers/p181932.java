0d4acc985abc795d9abb196718b684c4e0207d45
seung-hun2 <jungsh2328@naver.com>
2024-05-11 23:20:02 +0900

    프로그래머스 - 코드 처리하기

---
+++ src/programmers/p181932.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class p181932 {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') mode = 1 - mode;
+            else if (i % 2 == mode) answer += code.charAt(i);
+        }
+        return "".equals(answer) ? "EMPTY" : answer;
+    }
+
+}
