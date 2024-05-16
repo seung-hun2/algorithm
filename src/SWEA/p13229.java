e2ab34012d3a48f0ec3190d6d4d704e9c0a29105
seung-hun2 <jungsh2328@naver.com>
2024-05-17 00:20:17 +0900

    SWEA - 일요일

---
+++ src/SWEA/p13229.java
@@ -0,0 +1,29 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class p13229 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        String arr[] = {"SUN", "SAT", "FRI", "THU", "WED", "TUE", "MON"};
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String str = sc.next();
+            int ans = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].equals(str)) {
+                    ans = i;
+                    break;
+                }
+            }
+            if (ans == 0) {
+                ans = 7;
+            }
+            System.out.println("#" + test_case + " " + ans);
+        }
+
+    }
+
+}
