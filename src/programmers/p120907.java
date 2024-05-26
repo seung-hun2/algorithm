1622ffe10794fdc7d3d43df96e9a113fdd6aab81
seung-hun2 <jungsh2328@naver.com>
2024-05-26 20:16:01 +0900

    프로그래머스 - OX퀴즈

---
+++ src/programmers/p120907.java
@@ -0,0 +1,29 @@
+package programmers;
+
+public class p120907 {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        String[] s = new String[quiz.length];
+        for(int i=0;i<quiz.length;i++){
+            s = quiz[i].split(" ");
+
+            if(s[1].equals("-")){
+                if(Integer.parseInt(s[0]) - Integer.parseInt(s[2]) == Integer.parseInt(s[4])){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }
+
+            if(s[1].equals("+")){
+                if(Integer.parseInt(s[0]) + Integer.parseInt(s[2]) == Integer.parseInt(s[4])){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }
+        }
+        return answer;
+    }
+
+}
