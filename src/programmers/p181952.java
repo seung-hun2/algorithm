e655a3c98bcba5995c16487fd71a186e6fb1df0d
seung-hun2 <jungsh2328@naver.com>
2024-06-03 21:18:21 +0900

    프로그래머스 - 문자열 출력하기

---
+++ src/programmers/p181952.java
@@ -0,0 +1,12 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class p181952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+
+}
