6e0c43f3c2d1953872939680de69f9b16e31d5ea
seung-hun2 <jungsh2328@naver.com>
2024-06-11 23:01:08 +0900

    프로그래머스 - 옹알이 (1)

---
+++ src/programmers/p120956.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class p120956 {
+    public int solution(String[] babbling) {
+        String[] str = {"aya", "ye", "woo", "ma"};
+        int answer = 0;
+
+        for(int i=0; i<babbling.length; i++){
+            for(int j=0; j<str.length; j++){
+                babbling[i] = babbling[i].replace(str[j], " ");
+            }
+
+            if(babbling[i].trim().length() == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+}
