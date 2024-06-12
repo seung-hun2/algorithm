37656b6c50c2ca4c7bfc7c870d6b6ace9ba9d8cf
seung-hun2 <jungsh2328@naver.com>
2024-06-12 22:59:37 +0900

    프로그래머스 - 순위 검색

---
+++ src/programmers/p72412.java
@@ -0,0 +1,43 @@
+package programmers;
+
+import java.util.*;
+
+public class p72412 {
+    static HashMap<String, List<Integer>> map;
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        map = new HashMap<String, List<Integer>>();
+        for(int i=0;i<info.length;i++){
+            powerSet(0, "", info[i].split(" "));
+        }
+
+        for(String key : map.keySet()) Collections.sort(map.get(key));
+
+        for(int i=0;i<query.length;i++){
+            String[] arr = query[i].split(" and | ");
+            String key = String.join("", arr[0], arr[1], arr[2], arr[3]);
+            List<Integer> list = map.getOrDefault(key, new ArrayList<Integer>());
+
+            int score = Integer.parseInt(arr[4]);
+            int start = 0;
+            int end = list.size();
+            while(start<end){
+                int mid = (start + end) / 2;
+                if(list.get(mid) < score) start = mid + 1;
+                else end = mid;
+            }
+            answer[i] = list.size() - start;
+        }
+        return answer;
+    }
+
+    private static void powerSet(int idx, String s, String arr[]){
+        if(idx == 4){
+            map.computeIfAbsent(s, k -> new ArrayList<Integer>()).add(Integer.parseInt(arr[4]));
+            return;
+        }
+        powerSet(idx+1, s+arr[idx], arr);
+        powerSet(idx+1, s+"-", arr);
+    }
+
+}
