bb72b7b3e8e3daf47927cfbed1a6e504c3970595
seung-hun2 <jungsh2328@naver.com>
2024-06-21 18:58:21 +0900

    프로그래머스 - 영어가 싫어요

---
+++ src/programmers/p120894.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class p120894 {
+    public long solution(String numbers) {
+        String[] matchNumbers = {
+            "zero", "one", "two"
+            , "three", "four", "five"
+            , "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < matchNumbers.length; i++) {
+            numbers = numbers.replaceAll(matchNumbers[i], String.valueOf(i));
+        }
+        return Long.parseLong(numbers);
+    }
+
+}
